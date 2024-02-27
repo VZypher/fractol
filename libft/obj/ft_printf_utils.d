b114529d59ec16a64f2253a33ab0e4b9723c8b12
vince <fumif382@gmail.com>
2024-02-27 08:43:41 +0100

    cc

--- libft/obj/ft_printf_utils.d
+++
@@ -1,2 +0,0 @@
-obj/ft_printf_utils.o: ft_printf_utils.c ft_printf.h
-ft_printf.h:
--- obj/fractol_utils.d
+++
@@ -1,6 +0,0 @@
-obj/fractol_utils.o: fractol_utils.c fractol.h \
- minilibx-linux-master/mlx.h libft/libft.h libft/ft_printf.h
-fractol.h:
-minilibx-linux-master/mlx.h:
-libft/libft.h:
-libft/ft_printf.h:
